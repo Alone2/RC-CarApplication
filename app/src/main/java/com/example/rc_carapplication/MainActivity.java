475f03f0f8a49c10aafa165cab8200f6c0b62b0f
JoMee <jomethun>
2019-09-09 16:08:42 +0200

    added one button listener for down button

--- app/src/main/java/com/example/rc_carapplication/MainActivity.java
+++ app/src/main/java/com/example/rc_carapplication/MainActivity.java
@@ -3,20 +3,36 @@ package com.example.rc_carapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            Car car = new Car();
-            car.right();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        ImageView down = (ImageView) findViewById(R.id.downBtn);
+
+
+
+        down.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                Log.w("test","msg");
+                return false;
+            }
+        });
 
     }
+
 }
